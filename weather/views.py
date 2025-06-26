860b57c08b71381ac35947648b9babacc9614c4a
muaadh99 <muaadh.20@cse.mrt.ac.lk>
2025-06-26 20:35:20 +0530

    Additional Details Added

--- weather/views.py
+++ weather/views.py
@@ -11,13 +11,16 @@ def get_weather(city="Colombo"):
         return {
             "city": data["location"]["name"],
             "temp_c": data["current"]["temp_c"],
+            "feelslike_c": data["current"]["feelslike_c"],
             "humidity": data["current"]["humidity"],
             "wind_kph": data["current"]["wind_kph"],
             "uv": data["current"]["uv"],
+            "pressure_mb": data["current"]["pressure_mb"],
             "condition": data["current"]["condition"]["text"],
             "icon": data["current"]["condition"]["icon"],
         }
     return None
+
 
 def index(request):
     weather = get_weather()
