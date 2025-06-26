417f9be52d137751df06a5ce0dbc0b4d78a5ce70
muaadh99 <muaadh.20@cse.mrt.ac.lk>
2025-06-26 21:48:51 +0530

    Icons were removed

--- weather_reporter/settings.py
+++ weather_reporter/settings.py
@@ -128,8 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# For development only:
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'weather/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
